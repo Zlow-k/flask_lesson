c751f525a0eafe822e7fa1b03895f57d4ab0b57e
Zlow-k <yj.kasai@gmail.com>
2024-10-31 09:48:15 +0900

    第2章完了

--- apps/app.py
+++ apps/app.py
@@ -3,23 +3,18 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 # SQLAlchemyをインスタンス化
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
-def create_app():
+def create_app(config_key):
     # Flaskインスタンスを生成
     app = Flask(__name__)
     
-    app.config.from_mapping(
-        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI=\
-            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
-    )
+    app.config.from_object(config[config_key])
+    
     # SQLAlchemyと連携
     db.init_app(app)
     # Migrateと連携
