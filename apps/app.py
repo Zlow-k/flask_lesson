c61048976bc7c68328255c89944f93f67aea2025
Zlow-k <yj.kasai@gmail.com>
2024-11-01 23:54:14 +0900

    P152まで完了

--- apps/app.py
+++ apps/app.py
@@ -5,12 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from apps.config import config
 from dotenv import load_dotenv
+from flask_login import LoginManager
 
 load_dotenv() 
 
 # SQLAlchemyをインスタンス化
 db = SQLAlchemy()
 csrf = CSRFProtect()
+
+# LoginManagerをインスタンス化、未ログイン時のリダイレクト先を指定
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+# ログイン後の表示メッセージ
+login_manager.login_message = ""
+
+
 
 def create_app(config_key):
     # Flaskインスタンスを生成
@@ -25,8 +34,12 @@ def create_app(config_key):
     
     csrf.init_app(app)
     
-    from apps.crud import views as crud_views
+    login_manager.init_app(app)
     
+    from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    
+    from apps.auth import views as auth_views
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
     
     return app
--- apps/crud/models.py
+++ apps/crud/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from apps.app import db
-from werkzeug.security import generate_password_hash
+from apps.app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # db.Modelを継承したUserクラスを作成
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     # カラムの定義
     id = db.Column(db.Integer, primary_key=True)
@@ -25,3 +26,16 @@ class User(db.Model):
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
+    
+    # パスワードチェックする 
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    # メールアドレス重複をチェックする
+    def is_duplicate_email(self):
+        return User.query.filter_by(email=self.email).first() is not None
+
+# ログインしているユーザー情報を取得する関数
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
