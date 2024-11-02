bf5e64575b63a0343de1c1a409dfc269e140418c
Zlow-k <yj.kasai@gmail.com>
2024-11-02 11:34:45 +0900

    第1部完了

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,8 +1,8 @@
 from apps.app import db
-from apps.auth.forms import SignUoForm
+from apps.auth.forms import SignUoForm, LoginForm
 from apps.crud.models import User
 from flask import Blueprint, render_template, flash, url_for, redirect, request
-from  flask_login import login_user
+from flask_login import login_user, logout_user
 
 # Blueprintを使ってauthを生成する
 auth = Blueprint(
@@ -44,5 +44,25 @@ def signup():
         return redirect(next_)
     
     return render_template("auth/signup.html", form=form)
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # メールアドレスからユーザーを取得
+        user = User.query.filter_by(email=form.email.data).first()
         
+        # ユーザーが存在しパスワードが一致する場合は許可する
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user)
+            return redirect(url_for("crud.users"))
         
+        # 失敗時のメッセージを設定
+        flash("メールアドレスかパスワードが不正です。")
+    
+    return render_template("auth/login.html", form=form)
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -2,6 +2,7 @@ from apps.crud.forms import UserFrom
 from flask import Blueprint, render_template, redirect, url_for
 from apps.app import db
 from apps.crud.models import User
+from flask_login import login_required
 
 # Blueprintでcrudアプリを生成する
 crud = Blueprint(
@@ -13,15 +14,18 @@ crud = Blueprint(
 
 # indexエンドポイントを作成そindex.htmlを返す
 @crud.route("/")
+@login_required
 def index():
     return render_template("crud/index.html")
 
 @crud.route("/sql")
+@login_required
 def sql():
     db.session.query(User).all()
     return "コンソールログを確認してください"
 
 @crud.route("/users/new", methods=["GET", "POST"])
+@login_required
 def create_user():
     form = UserFrom()
     if form.validate_on_submit():
@@ -37,12 +41,14 @@ def create_user():
     return render_template("crud/create.html", form=form)
 
 @crud.route("/users")
+@login_required
 def users():
     """ユーザーの一覧を取得する"""
     users = User.query.all()
     return render_template("crud/index.html", users=users)
 
 @crud.route("/users/<user_id>", methods=["GET", "POST"])
+@login_required
 def edit_user(user_id):
     form = UserFrom()
     
@@ -61,6 +67,7 @@ def edit_user(user_id):
     return render_template("crud/edit.html", user=user, form=form)
 
 @crud.route("/users/<user_id>/delete", methods=["POST"])
+@login_required
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
