e7b645a3374dfc1d4198906557e3a54e9d1f77da
Zlow-k <yj.kasai@gmail.com>
2024-11-07 09:12:58 +0900

    P247まで完成

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -19,6 +19,7 @@ from flask import (
     redirect,
     url_for,
     flash,
+    request,
 )
 from PIL import Image
 from sqlalchemy.exc import SQLAlchemyError
@@ -238,3 +239,59 @@ def delete_image(image_id):
         db.session.rollback()
         
     return redirect(url_for("detector.index"))
+
+@dt.route("/images/search", methods=["GET"])
+def search():
+    user_images = db.session.query(User, UserImage).join(
+        UserImage, User.id == UserImage.user_id
+    )
+    
+    # 検索ワードを取得
+    search_text = request.args.get("search")
+    user_image_tag_dict = {}
+    filtered_user_images = []
+    
+    # user_imagesに紐づくタグ情報を検索
+    for user_image in user_images:
+        # 検索ワードが空の場合はすべてのタグを取得
+        if not search_text:
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id ==user_image.UserImage.id)
+                .all()
+            )
+        else:
+            # 検索ワードで絞込んだタグを取得
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+                .filter(UserImageTag.tag_name.like("%" + search_text + "%"))
+                .all()
+            )
+            
+            # タグが見つからない場合画像を返さない
+            if not user_image_tags:
+                continue
+            
+            # タグがある場合は取得しなおす
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+                .all()
+            )
+        
+        # 辞書にタグ情報をセットする    
+        user_image_tag_dict[user_image.UserImage.id] = user_image_tags
+        
+        filtered_user_images.append(user_image)
+    
+    delete_form = DeleteForm()
+    detector_form = DetectorForm()
+    
+    return render_template(
+        "detector/index.html",
+        user_images=filtered_user_images,
+        user_image_tag_dict=user_image_tag_dict,
+        delete_form=delete_form,
+        detector_form=detector_form,
+    )
